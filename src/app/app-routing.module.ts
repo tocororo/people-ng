3c73d31d137352d8d3af2ba54eaf4674120b05a3
luiso <lecc628@gmail.com>
2022-02-06 17:46:21 -0500

    Do the initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+	// {
+	// 	path: 'people',
+	// 	loadChildren: () => import('./people/people.module').then(mod => mod.PeopleModule)
+	// },
+	{
+		path: '',
+		component: HomeComponent,
+	},
+	{
+		path: '**',
+		component: PageNotFoundComponent
+	}
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+export class AppRoutingModule
+{ }
---
+++ src/app/app.module.ts
@@ -0,0 +1,84 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RecaptchaModule } from 'ng-recaptcha';
+import { MarkdownModule } from 'ngx-markdown';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { Environment } from 'toco-lib';
+import { environment } from 'src/environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader
+{
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		PageNotFoundComponent,
+		FooterComponent,
+		ContactComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		TranslateModule.forRoot({
+		  loader: {
+			  provide: TranslateLoader,
+			  useFactory: (createTranslateLoader),
+			  deps: [HttpClient]
+		  }
+		}),
+		ReactiveFormsModule,
+		FlexLayoutModule,
+		RecaptchaModule,
+		MarkdownModule.forRoot({
+			loader: HttpClient
+		}),
+
+		MatButtonModule,
+		MatTooltipModule,
+		MatMenuModule,
+		MatIconModule,
+		MatToolbarModule,
+		MatDividerModule,
+		MatCardModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatSnackBarModule,
+
+		AppRoutingModule
+	],
+	providers: [
+		{ provide: Environment, useValue: environment }
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule
+{ }
