b2fc85d0569bcfccc522d37cad06c3957414e14e
Rafael Martinez Estevez <malayibiri@gmail.com>
2023-03-03 02:43:07 -0500

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,66 +1,64 @@
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
-import { HttpClientModule, HttpClient } from "@angular/common/http";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { MarkdownModule } from "ngx-markdown";
 
-import { MatButtonModule } from "@angular/material/button";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatCardModule } from "@angular/material/card";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatPaginatorModule, MatRadioModule, MatSidenavModule } from "@angular/material";
-import { MatTabsModule } from "@angular/material/tabs";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from "@angular/material/chips";
+import { MatDividerModule } from "@angular/material/divider";
 import { MatExpansionModule } from "@angular/material/expansion";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatTableModule } from "@angular/material/table";
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
+import { OAuthModule, OAuthStorage } from "angular-oauth2-oidc";
 import {
   AngularMaterialModule,
   AuthenticationModule,
   CoreModule,
-  Environment,
-  SearchModule,
+  Environment, OrganizationServiceNoAuth, SearchModule,
   SearchService,
   SourceServiceNoAuth,
   StaticsModule,
-  TocoFormsModule,
-  OrganizationServiceNoAuth,
+  TocoFormsModule
 } from "toco-lib";
-import { OAuthModule, OAuthStorage } from "angular-oauth2-oidc";
 
 import { allowedURLS, environment } from "src/environments/environment";
 
+import { NgxDropzoneModule } from "node_modules/ngx-dropzone";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { PageNotFoundPeopleComponent } from "./page-not-found-people/page-not-found-people.component";
 import { FooterComponent } from "./footer/footer.component";
-import { SearchComponent } from "./search/search.component";
-import { SearchListComponent } from "./search-list/search-list.component";
-import { PeopleViewComponent } from "./people-view/people-view.component";
-import { MainlayoutComponent } from "./layout/mainlayout/mainlayout.component";
-import { PeopleLayoutComponent } from "./layout/people-layout/people-layout.component";
-import { GeneralTabComponent } from "./people-view/general-tab/general-tab.component";
 import { HeaderComponent } from "./header/header.component";
 import { MenuItemComponent } from "./header/menu-item/menu-item.component";
 import { MenuComponent } from "./header/menu/menu.component";
+import { HomeComponent } from "./home/home.component";
 import { ImportPeopleComponent } from "./import-people/import-people.component";
-import { NgxDropzoneModule } from "node_modules/ngx-dropzone";
-import { SelectOrgComponent } from "./select-org/select-org.component";
 import { OrgDialogComponent } from "./import-people/org-dialog/org-dialog.component";
+import { MainlayoutComponent } from "./layout/mainlayout/mainlayout.component";
+import { PeopleLayoutComponent } from "./layout/people-layout/people-layout.component";
 import { OrgService } from "./org.service";
+import { PageNotFoundPeopleComponent } from "./page-not-found-people/page-not-found-people.component";
+import { GeneralTabComponent } from "./people-view/general-tab/general-tab.component";
+import { PeopleViewComponent } from "./people-view/people-view.component";
+import { SearchListComponent } from "./search-list/search-list.component";
+import { SearchComponent } from "./search/search.component";
+import { SelectOrgComponent } from "./select-org/select-org.component";
 
 export function storageFactory(): OAuthStorage {
   return localStorage;
@@ -90,6 +88,7 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
     ImportPeopleComponent,
     SelectOrgComponent,
     OrgDialogComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
