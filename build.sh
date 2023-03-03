f37922345c975aca15e7a73e03890f35136bad22
Rafael Martinez Estevez <malayibiri@gmail.com>
2023-03-03 02:39:21 -0500

    s

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-ng build  --prod --aot --preserve-symlinks --build-optimizer=true  --verbose --project=catalog --baseHref https://cuba-personas.sceiba.org --deploy-url https://cuba-personas.sceiba.org/
+ng build  --prod --aot --preserve-symlinks --build-optimizer=true  --verbose --baseHref https://cuba-personas.sceiba.org --deploy-url https://cuba-personas.sceiba.org/
