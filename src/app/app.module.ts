a66a2dc686708134f3d984456f1c45a521af5438
Rafael Martinez Estevez <malayibiri@gmail.com>
2023-10-21 14:00:21 -0400

    running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,35 +9,19 @@ import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { MarkdownModule } from "ngx-markdown";
 
 import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatRadioModule } from "@angular/material/radio";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from "@angular/material/chips";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatListModule } from "@angular/material/list";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatTableModule } from "@angular/material/table";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatTooltipModule } from "@angular/material/tooltip";
 
 import { OAuthModule, OAuthStorage } from "angular-oauth2-oidc";
 import {
-  AngularMaterialModule,
   AuthenticationModule,
   CoreModule,
-  Environment, OrganizationServiceNoAuth, SearchModule,
+  Environment, OrganizationServiceNoAuth,
+  OrganizationsModule,
+  SearchModule,
   SearchService,
   SourceServiceNoAuth,
   StaticsModule,
-  TocoFormsModule, OrganizationsModule
+  TocoFormsModule
 } from "toco-lib";
 
 import { allowedURLS, environment } from "src/environments/environment";
@@ -51,7 +35,9 @@ import { HeaderComponent } from "./header/header.component";
 import { MenuItemComponent } from "./header/menu-item/menu-item.component";
 import { MenuComponent } from "./header/menu/menu.component";
 import { HomeComponent } from "./home/home.component";
+import { CsvTableComponent } from './import-people/csv-table/csv-table.component';
 import { ImportPeopleComponent } from "./import-people/import-people.component";
+import { JsonTableComponent } from './import-people/json-table/json-table.component';
 import { OrgDialogComponent } from "./import-people/org-dialog/org-dialog.component";
 import { MainlayoutComponent } from "./layout/mainlayout/mainlayout.component";
 import { PeopleLayoutComponent } from "./layout/people-layout/people-layout.component";
@@ -62,8 +48,7 @@ import { PeopleViewComponent } from "./people-view/people-view.component";
 import { SearchListComponent } from "./search-list/search-list.component";
 import { SearchComponent } from "./search/search.component";
 import { SelectOrgComponent } from "./select-org/select-org.component";
-import { JsonTableComponent } from './import-people/json-table/json-table.component';
-import { CsvTableComponent } from './import-people/csv-table/csv-table.component';
+import { SharedModule } from "./shared/shared.module";
 
 export function storageFactory(): OAuthStorage {
   return localStorage;
@@ -115,27 +100,10 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
         MarkdownModule.forRoot({
             loader: HttpClient,
         }),
-        MatButtonModule,
-        MatTooltipModule,
-        MatMenuModule,
-        MatIconModule,
-        MatToolbarModule,
-        MatDividerModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSnackBarModule,
-        MatSidenavModule,
-        MatPaginatorModule,
-        MatTabsModule,
-        MatChipsModule,
-        MatExpansionModule,
-        MatListModule,
+
         TocoFormsModule,
-        MatRadioModule,
-        MatCheckboxModule,
+        SharedModule,
         FormsModule,
-        AngularMaterialModule,
         CoreModule,
         StaticsModule,
         TocoFormsModule,
